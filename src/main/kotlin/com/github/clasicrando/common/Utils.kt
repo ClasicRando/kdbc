0e7e60765b5455ea8dec8c00f12bb3f96292bbc0
Steven Thomson <steventhomson9@gmail.com>
2023-11-23 00:03:29 -0500

    feat: add selectLoop method for looping until a break condition is met

--- src/main/kotlin/com/github/clasicrando/common/Utils.kt
+++ src/main/kotlin/com/github/clasicrando/common/Utils.kt
@@ -1,5 +1,8 @@
 package com.github.clasicrando.common
 
+import kotlinx.coroutines.selects.SelectBuilder
+import kotlinx.coroutines.selects.select
+
 val Byte.Companion.ZERO: Byte get() = 0
 
 inline fun <T> Result<T>.mapError(block: (Throwable) -> Throwable): Result<T> {
@@ -7,4 +10,19 @@ inline fun <T> Result<T>.mapError(block: (Throwable) -> Throwable): Result<T> {
         return this
     }
     return Result.failure(block(this.exceptionOrNull()!!))
+}
+
+sealed interface Loop {
+    data object Continue : Loop
+    data object Break : Loop
+}
+
+suspend inline fun selectLoop(crossinline block: SelectBuilder<Loop>.() -> Unit) {
+    while (true) {
+        val loop = select(builder = block)
+        when (loop) {
+            is Loop.Continue -> continue
+            is Loop.Break -> break
+        }
+    }
 }
