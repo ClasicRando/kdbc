0dee24e28a859b77a6c779ac92819e7e5302dcb7
Steven Thomson <steventhomson9@gmail.com>
2023-12-25 12:10:10 -0500

    feat: create helper class for test connection creation

---
+++ src/test/kotlin/com/github/clasicrando/postgresql/PgConnectionHelper.kt
@@ -0,0 +1,30 @@
+package com.github.clasicrando.postgresql
+
+import com.github.clasicrando.common.pool.ConnectionPool
+import com.github.clasicrando.common.pool.PoolOptions
+import kotlinx.coroutines.CoroutineScope
+
+object PgConnectionHelper {
+    private val defaultConnectOptions = PgConnectOptions(
+        host = "localhost",
+        port = 5432U,
+        username = "postgres",
+        password = System.getenv("PG_TEST_PASSWORD"),
+        applicationName = "KdbcTests",
+    )
+    private val defaultPoolOptions = PoolOptions(maxConnections = 1)
+
+    suspend fun defaultConnection(scope: CoroutineScope): PgConnection {
+        return PostgresqlConnectionBuilder.createConnection(
+            scope = scope,
+            connectOptions = defaultConnectOptions,
+        )
+    }
+
+    fun defaultConnectionPool(): ConnectionPool {
+        return PostgresqlConnectionBuilder.createConnectionPool(
+            poolOptions = defaultPoolOptions,
+            connectOptions = defaultConnectOptions,
+        )
+    }
+}
--- src/test/kotlin/com/github/clasicrando/postgresql/authentication/TestSaslAuthFlow.kt
+++ src/test/kotlin/com/github/clasicrando/postgresql/authentication/TestSaslAuthFlow.kt
@@ -1,31 +1,25 @@
 package com.github.clasicrando.postgresql.authentication
 
-import com.github.clasicrando.postgresql.PgConnectOptions
-import com.github.clasicrando.postgresql.PgConnectionImpl
-import com.github.clasicrando.postgresql.pool.PgConnectionFactory
+import com.github.clasicrando.postgresql.PgConnection
+import com.github.clasicrando.postgresql.PgConnectionHelper
+import kotlinx.coroutines.job
 import kotlinx.coroutines.runBlocking
 import kotlin.test.Test
 
 class TestSaslAuthFlow {
-    private val defaultConnectOptions = PgConnectOptions(
-        host = "localhost",
-        port = 5432U,
-        username = "postgres",
-        password = System.getenv("PG_TEST_PASSWORD"),
-        applicationName = "TestSaslAuthFlow",
-    )
-    private fun factory(connectOptions: PgConnectOptions) = PgConnectionFactory(connectOptions)
-
     @Test
     fun `saslAuthFlow should succeed when valid login`(): Unit = runBlocking {
-        var connection: PgConnectionImpl? = null
+        var connection: PgConnection? = null
         try {
-            connection = factory(defaultConnectOptions).create(this) as PgConnectionImpl
+            connection = PgConnectionHelper.defaultConnection(this)
             connection.sendQuery("SELECT 1")
         } catch (ex: Throwable) {
             throw ex
         } finally {
             connection?.close()
+        }
+        for (child in this.coroutineContext.job.children) {
+            println(child)
         }
     }
 }
