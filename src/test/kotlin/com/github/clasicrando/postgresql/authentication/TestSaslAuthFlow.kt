84636d8cd46c6ce9d7388470a1f17fd48d862465
Steven Thomson <steventhomson9@gmail.com>
2023-12-26 22:46:18 -0500

    chore: remove unused imports

--- src/test/kotlin/com/github/clasicrando/postgresql/authentication/TestSaslAuthFlow.kt
+++ src/test/kotlin/com/github/clasicrando/postgresql/authentication/TestSaslAuthFlow.kt
@@ -1,9 +1,7 @@
 package com.github.clasicrando.postgresql.authentication
 
 import com.github.clasicrando.common.connection.use
-import com.github.clasicrando.postgresql.PgConnection
 import com.github.clasicrando.postgresql.PgConnectionHelper
-import kotlinx.coroutines.job
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable
 import kotlin.test.Test
